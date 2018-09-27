1081e599890eb8b51a6136cc8b1e488bba6d54c4
ZeroWu <35825670+ZeroWu1412@users.noreply.github.com>
2018-09-26 15:00:09 -1000

    system call (#8)
    * Update syscall.h
    
    * Update syscall.h
    
    * Update syscall.h
    
    * Update syscall.c
    
    * Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -7,18 +7,86 @@
 #include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
+void sys_exit (int);
 
-void
+void 
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+// in case of invalid memory access, fail and exit.
+static void fail_invalid_access(void) {
+  if (lock_held_by_current_thread(&filesys_lock))
+    lock_release (&filesys_lock);
+
+  sys_exit (-1);
+  NOT_REACHED();
+}
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int syscall_number;
+  ASSERT( sizeof(syscall_number) == 4 ); // assuming x86
+
+  // The system call number is in the 32-bit word at the caller's stack pointer.
+  memread_user(f->esp, &syscall_number, sizeof(syscall_number));
+  _DEBUG_PRINTF ("[DEBUG] system call, number = %d!\n", syscall_number);
+
+  // Store the esp, which is needed in the page fault handler.
+  // refer to exception.c:page_fault() (see manual 4.3.3)
+  thread_current()->current_esp = f->esp;
+
+  // Dispatch w.r.t system call number
+  // SYS_*** constants are defined in syscall-nr.h
+  switch (syscall_number) {
+  case SYS_HALT: // 0
+    {
+      sys_halt();
+      NOT_REACHED();
+      break;
+    }
+
+  case SYS_EXIT: // 1
+    {
+      int exitcode;
+      memread_user(f->esp + 4, &exitcode, sizeof(exitcode));
+
+      sys_exit(exitcode);
+      NOT_REACHED();
+      break;
+    }
+#endif
+      
+  /* unhandled case */
+  default:
+    printf("[ERROR] system call %d is unimplemented!\n", syscall_number);
+
+    // ensure that waiting (parent) process should wake up and terminate.
+    sys_exit(-1);
+    break;
+  }
+}
+
+oid sys_halt(void) {
+  shutdown_power_off();
+}
+
+void sys_exit(int status) {
+  printf("%s: exit(%d)\n", thread_current()->name, status);
+  
+  // The process exits.
+  // wake up the parent process (if it was sleeping) using semaphore,
+  // and pass the return code.
+  struct process_control_block *pcb = thread_current()->pcb;
+  if(pcb != NULL) {
+    pcb->exitcode = status;
+  }
+  else {
+    // pcb == NULL probably means that previously
+    // page allocation has failed in process_execute()
+  }
+  thread_exit();
 }
 
 /* The kernel must be very careful about doing so, because the user can pass
--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -1,6 +1,9 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include "userprog/process.h"
 
 void syscall_init (void);
+
+void sys_exit (int);
 
 #endif /* userprog/syscall.h */
